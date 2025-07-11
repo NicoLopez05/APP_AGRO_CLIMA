c6ed68d05890181e24ca71ef095b4cd92c5cf04f
NicoLopez05 <167824882+NicoLopez05@users.noreply.github.com>
2025-07-11 14:23:10 -0500

    Delete AGRO_CLIMA_APP directory

--- AGRO_CLIMA_APP/tests/test_unit_backend.py
+++
@@ -1,4 +0,0 @@
-def test_sumar_valores_sensor():
-    def sumar(a, b):
-        return a + b
-    assert sumar(2, 3) == 5
--- AGRO_CLIMA_APP/tests/test_utils.py
+++
@@ -1,5 +0,0 @@
-def parse_tipo_sensor(tipo):
-    return tipo.upper()
-
-def test_parse_tipo_sensor():
-    assert parse_tipo_sensor("lluvia") == "LLUVIA"
